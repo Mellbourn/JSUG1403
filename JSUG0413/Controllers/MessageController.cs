6d6c95d26e04c01e64671d0954c95674be630d1e
Klas Mellbourn <klas@mellbourn.net>
2014-01-15 21:56:29 +0100

    added REST api for Messages

---
+++ JSUG0413/Controllers/MessageController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using JSUG0413.Models;
+
+namespace JSUG0413.Controllers
+{
+    public class MessageController : ApiController
+    {
+        private JSUG0413Context db = new JSUG0413Context();
+
+        // GET api/Message
+        public IQueryable<Message> GetMessages()
+        {
+            return db.Messages;
+        }
+
+        // GET api/Message/5
+        [ResponseType(typeof(Message))]
+        public async Task<IHttpActionResult> GetMessage(int id)
+        {
+            Message message = await db.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(message);
+        }
+
+        // PUT api/Message/5
+        public async Task<IHttpActionResult> PutMessage(int id, Message message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != message.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(message).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MessageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Message
+        [ResponseType(typeof(Message))]
+        public async Task<IHttpActionResult> PostMessage(Message message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Messages.Add(message);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = message.Id }, message);
+        }
+
+        // DELETE api/Message/5
+        [ResponseType(typeof(Message))]
+        public async Task<IHttpActionResult> DeleteMessage(int id)
+        {
+            Message message = await db.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            db.Messages.Remove(message);
+            await db.SaveChangesAsync();
+
+            return Ok(message);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MessageExists(int id)
+        {
+            return db.Messages.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ JSUG0413/Models/JSUG0413Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JSUG0413.Models
+{
+    public class JSUG0413Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public JSUG0413Context() : base("name=JSUG0413Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<JSUG0413.Models.Message> Messages { get; set; }
+    
+    }
+}
